7d7d1463dd069bba4cc0cdbdf5f10af1bcf4cf24
diegollanespasos <diego.llanes@theksquaregroup.com>
2021-05-07 19:09:54 -0500

    modified the PORT to 8080

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const express = require('express');
 const transactions = require('./routes/transactions');
 const connectDB = require('./config/db');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 dotenv.config({ path: './config/config.env' });
 
 connectDB();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/transactions', transactions);
 
-app.listen(process.env.PORT, () => console.log('Listening on PORT 5000'));
+app.listen(process.env.PORT, () => console.log(`Server listening on PORT: ${process.env.PORT}`));
